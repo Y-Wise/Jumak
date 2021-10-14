0c013e72305701d596a8cb1298db4bc9602e936f
dkrkek96@soongsil.ac.kr <66158433+Y-Wise@users.noreply.github.com>
2021-10-14 19:37:00 +0900

    backup

---
+++ Graduation_YEM/GraduationProject-YEM/Graduation_YEM/Assets/02.Scripts/ManageScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ManageScene : MonoBehaviour
+{
+    public Image backgroundImage;
+    private void OnMouseUp()
+    {
+        //if(GetComponent)
+        //로그인씬(씬2)으로 전환
+        SceneManager.LoadScene("02_Login");
+    }
+}
